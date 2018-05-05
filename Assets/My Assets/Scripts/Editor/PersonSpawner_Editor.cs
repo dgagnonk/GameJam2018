21e867e82e3494a818572003f3498311df78abe6
Davide De Simone <ddesimone1@gmail.com>
2018-05-04 23:41:07 -0400

    fixed namespace mismatch

--- Assets/My Assets/Scripts/Editor/PersonSpawner_Editor.cs
+++ Assets/My Assets/Scripts/Editor/PersonSpawner_Editor.cs
@@ -4,37 +4,41 @@ using UnityEngine;
 using UnityEditor;
 
 
-//Custom inspector for personSpawner
-[CustomEditor(typeof(PersonSpawner))]
-public class PersonSpawnerEditor : Editor
+namespace GameJam2018
 {
-    bool setupFoldout;
-    public override void OnInspectorGUI()
+
+    //Custom inspector for personSpawner
+    [CustomEditor(typeof(PersonSpawner))]
+    public class PersonSpawnerEditor : Editor
     {
-        PersonSpawner personSpawnerTarget = (PersonSpawner) target;
-
-        personSpawnerTarget.randomSpawn = EditorGUILayout.Toggle("Random Spawn", personSpawnerTarget.randomSpawn);
-
-        if(personSpawnerTarget.randomSpawn)
+        bool setupFoldout;
+        public override void OnInspectorGUI()
         {
-            personSpawnerTarget.numPeople = EditorGUILayout.IntField("Number of NPCs", personSpawnerTarget.numPeople);
+            PersonSpawner personSpawnerTarget = (PersonSpawner)target;
+
+            personSpawnerTarget.randomSpawn = EditorGUILayout.Toggle("Random Spawn", personSpawnerTarget.randomSpawn);
+
+            if (personSpawnerTarget.randomSpawn)
+            {
+                personSpawnerTarget.numPeople = EditorGUILayout.IntField("Number of NPCs", personSpawnerTarget.numPeople);
+            }
+            else
+            {
+                personSpawnerTarget.columns = EditorGUILayout.IntField("NPC Columns", personSpawnerTarget.columns);
+                personSpawnerTarget.rows = EditorGUILayout.IntField("NPC Rows", personSpawnerTarget.rows);
+            }
+
+
+            setupFoldout = EditorGUILayout.Foldout(setupFoldout, "Additional Settings", true);
+            if (setupFoldout)
+            {
+                personSpawnerTarget.boundaries = (SpawnBoundaries)EditorGUILayout.ObjectField("Boundaries Script", personSpawnerTarget.boundaries, typeof(SpawnBoundaries), true);
+
+                personSpawnerTarget.peopleParent = (Transform)EditorGUILayout.ObjectField("People Parent", personSpawnerTarget.peopleParent, typeof(Transform), true);
+
+                personSpawnerTarget.personPrefab = (GameObject)EditorGUILayout.ObjectField("Person Prefab", personSpawnerTarget.personPrefab, typeof(GameObject), true);
+            }
+
         }
-        else
-        {
-            personSpawnerTarget.columns = EditorGUILayout.IntField("NPC Columns", personSpawnerTarget.columns);
-            personSpawnerTarget.rows = EditorGUILayout.IntField("NPC Rows", personSpawnerTarget.rows);
-        }
-
-
-        setupFoldout = EditorGUILayout.Foldout(setupFoldout, "Additional Settings", true);
-        if(setupFoldout)
-        {
-            personSpawnerTarget.boundaries = (SpawnBoundaries)EditorGUILayout.ObjectField("Boundaries Script", personSpawnerTarget.boundaries, typeof(SpawnBoundaries), true);
-
-            personSpawnerTarget.peopleParent = (Transform)EditorGUILayout.ObjectField("People Parent", personSpawnerTarget.peopleParent, typeof(Transform), true);
-
-            personSpawnerTarget.personPrefab = (GameObject)EditorGUILayout.ObjectField("Person Prefab", personSpawnerTarget.personPrefab, typeof(GameObject), true);
-        }
-
     }
 }
