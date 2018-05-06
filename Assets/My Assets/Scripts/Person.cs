7a581ab692d562c20fad6cb24f54184206109102
Davide De Simone <ddesimone1@gmail.com>
2018-05-05 21:05:19 -0400

    integrated person animations

--- Assets/My Assets/Scripts/Person.cs
+++ Assets/My Assets/Scripts/Person.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Person : MonoBehaviour {
 
-
-    /*
-     *  PERSON TYPES
-     *  A person type is an int between 1 and 4 (inclusive).
-     */
-
-    public int HowManyPersonTypes = 4;
-    public int PersonType;
-
-	void Start ()
+namespace GameJam2018
+{
+    public class Person : MonoBehaviour
     {
-        PersonType = (int)Random.Range(1, HowManyPersonTypes + 1); // +1 here because max is exclusive
-	}
-	
+
+
+        /*
+         *  PERSON TYPES
+         *  A person type is an int between 1 and 4 (inclusive).
+         */
+
+        public int HowManyPersonTypes = 4;
+        public int PersonType;
+
+        private Animator _animator;
+
+        void Start()
+        {
+            PersonType = (int)Random.Range(1, HowManyPersonTypes + 1); // +1 here because max is exclusive
+            this._animator = this.GetComponentInChildren<Animator>();
+
+            this._animator.SetBool("Walking", true);
+            this._animator.SetBool("Listening", false);
+            this._animator.SetBool("Talking", false);
+
+
+            //this._animator.runtimeAnimatorController.
+        }
+
+    }
 }
+
+
