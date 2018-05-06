2e7643f511811ac019091b6a55e809284e8737a7
GGervais54 <gibson.gervais1@gmail.com>
2018-05-05 20:07:57 -0400

    added animation script

---
+++ Assets/My Assets/Prefabs/PersonAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonAnimation : MonoBehaviour {
+
+    public Animator p_Animation;
+    public Rigidbody person;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        p_Animation.SetBool("Walking", false);
+        p_Animation.SetBool("Talking", false);
+        p_Animation.SetBool("Listening", false);
+
+        if (person.velocity.x != 0 || person.velocity.z != 0)
+            p_Animation.SetBool("Walking", true);
+        else
+            p_Animation.SetBool("Listening", true);
+
+    }
+}
