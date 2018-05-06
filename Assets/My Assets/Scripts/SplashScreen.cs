ebd19a2a13a1d5a51e66a47da5305e831bfef0ce
Davide De Simone <ddesimone1@gmail.com>
2018-05-05 23:09:44 -0400

    added splash screen and configured build

---
+++ Assets/My Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XInputDotNetPure;
+
+
+namespace GameJam2018
+{
+    public class SplashScreen : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        } 
+
+        // Update is called once per frame
+        void Update()
+        {
+            for(int i=0; i < 4; i++)
+            {
+                GamePadState state = GamePad.GetState((PlayerIndex)i);
+                if(state.Buttons.Start == ButtonState.Pressed)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                }
+            }
+            
+        }
+    }
+
+}
